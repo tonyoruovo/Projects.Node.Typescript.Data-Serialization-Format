1a2e47686b9cdc8d2b4f51f3f63a204e34dd0b7f
MathAid <tonyoruovo@gmail.com>
2023-09-14 01:24:54 +0100

    Sill empty

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -1,5 +1,14 @@
-// import parser from "./parser.js";
+// // import parser from "./parser.js";
 
-// export function getCmd<T extends parser.Type, C extends parser.Command>(d: parser.Direction, t: T): C | undefined {
+// import { RequestOptions } from "node:https";
+// import { type } from "node:os";
 
+// // export function getCmd<T extends parser.Type, C extends parser.Command>(d: parser.Direction, t: T): C | undefined {
+
+// // }
+// type Razor<T> = {
+//     [P in keyof T]: (c: P) => string;
 // }
+// class RazorII implements Razor<RequestOptions> {
+// }
+// const x = new RazorII().servername(true);
