f1df5298229410dcce6fc0743ca6b0c0d099b686
tonyoruovo <tonyoruovo@gmail.com>
2024-03-01 09:17:29 +0100

    Initial commit: Scafolding code

---
+++ src/mem/json.ts
@@ -0,0 +1,15 @@
+import mem from "./mem.js";
+
+namespace json {
+    const Type = mem.token.GType;
+    export interface Syntax extends mem.parser.GSyntax<mem.token.GType<string>, Command> {
+    };
+    export type Lexer = mem.parser.MutableLexer<mem.token.GToken<string>, Syntax, string>;
+    export type Command = mem.parser.GCommand<mem.token.GToken<string>, Expression, Syntax, Lexer, Parser>;
+    export type Parser = mem.parser.PrattParser<Expression, Syntax, string>;
+    export type Expression = mem.expression.GExpression<Serializer>;
+    export interface Serializer extends mem.expression.GFormat<Expression, string> {
+        (data?: Expression | string): void;
+    }
+}
+export default json;
